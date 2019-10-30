85d6f3e1a7e9491ac2148761c9f46df75599a2b0
William Yang <williamyang0903@hotmail.com>
2019-10-29 20:15:13 -0700

    one final push

--- lesson-6/myPortfolio/js/script.js
+++ lesson-6/myPortfolio/js/script.js
@@ -1,19 +1,8 @@
 $(document).ready(function(){
     $('#slides').superslides({
 
-        animation: 'fade' ,
-        play: 5000,
-        pagination: false
     });
 
-    var typed = new Typed(".typed", {
-        strings: ["I am a Software Developer", "I am a Trojan", "I am an Scoper"],
-		smartBackspace: true,
-		typeSpeed: 70,
-		backSpeed: 35,
-		loop: true,
-		startDelay: 1000,
-		showCursor: false
-    });
+
        
 });
